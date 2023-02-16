958f5fe8a976724cb4f1f2c8ff6b15f67ad730c6
FCA <fca@inet.uady.mx>
2023-02-15 20:02:03 -0600

    Agregando proyecto

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+
+df = px.data.gapminder()
+st.dataframe(df)
+listaPaises = df["country"].unique().tolist()
+#st.write(listaPaises)
+
+pais="Canada"
+with st.sidebar:
+    pais = st.selectbox('Paises', listaPaises)
+    st.write('Has seleccionado:', pais)
+
+datosPais = df.query("country == '" + pais+"'")
+fig = px.bar(datosPais, x='year', y='pop')
+st.plotly_chart(fig, use_container_width=True)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    st.header("Hola desde Streamlit!")
+    st.subheader("Probando ..1..2..3")
+    st.write("Hola soy Heisler")
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
